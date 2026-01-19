b9ad2aff32e7770ab731dfb78b19d2193cfbf2c5
iwannabeluchi <13817165917@163.com>
2026-01-19 22:15:00 +0900

    test stash add?Change by undo(Secxond commit)

---
+++ src/main/java/test/example/demo2/AddFile.java
@@ -0,0 +1,17 @@
+
+package test.example.demo2;
+
+public class AddFile {
+    // 这是一个用于演示的新增文件
+    public String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+
+    public int stash_multiply(int a, int i) {
+        for(int j=0;j<i-1;j++){
+            a+=a;
+        }
+        return a;
+    }
+    
+}
